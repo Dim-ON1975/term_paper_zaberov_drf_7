c155193aa8ec94a27ccbacd75fb0a78ccbbcba58
zaberov.dv <zaberov.dv@internet.ru>
2024-04-09 20:45:13 +0300

    Создан проект, установлены необходимые зависимости, добавлено приложение "Пользователи": CRUD, авторизация по токену.

---
+++ users/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='электронная почта')
+    email_verify = models.BooleanField(default=False, verbose_name='верификация почты')
+    phone = models.CharField(max_length=35, **NULLABLE, verbose_name='телефон')
+    settlement = models.CharField(max_length=100, verbose_name='населённый пункт', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/%Y/%m/%d/', **NULLABLE, verbose_name='аватар')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
