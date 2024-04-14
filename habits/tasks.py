8a7bb01773661bacd9ae2d3f75bb81b57b7f9610
zaberov.dv <zaberov.dv@internet.ru>
2024-04-14 18:08:26 +0300

    Курсовая работа № 7 выполнена - внесены незначительные изменения в логику вычисления дней выполнения задач.

--- habits/tasks.py
+++ habits/tasks.py
@@ -20,8 +20,8 @@ def send_message_habit_telegram(*args, **kwargs):
 
         frequency = values.frequency  # периодичность выполнения
         # дни недели для выполнения задачи, отсчёт от понедельника (0)
-        days_habit_list = [x for x in range(0, 7, frequency)]
-        week_day = datetime.datetime.now().weekday()  # текущий день недели 0-6
+        days_habit_list = [x for x in range(1, 8, frequency)]
+        week_day = datetime.datetime.now().weekday() + 1  # текущий день недели 1-7
 
         # Отправка сообщений при совпадении дня недели (0-6) и времени
         if week_day in days_habit_list and time_habit == time_now:
